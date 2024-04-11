0a3831b3ef11ade4216b6005e4933871a35f020d
Cristian Chacha <cristianchacha@gmail.com>
2024-04-11 18:25:21 +0200

    Added loop

--- calculadora.py
+++ calculadora.py
@@ -17,30 +17,36 @@ def serie_fibonaci(rango):
             print()
         n_a = n_b
         n_b = resultado
+    print()
 
 print("Calculadora")
-opciones = input("opcion 1:sumar\nopcion 2: restar\nopcion 5:fibonacci\n")
+ejecutar = True
+while(ejecutar):
+    opciones = input("opcion 1:sumar\nopcion 2: restar\nopcion 5:fibonacci\nopcion 0:salir\n")
 
-opciones = int(opciones)
-if opciones == 5:
-    numero1 = input("Introducir cantidad de numeros para la serie: ")   
-    numero1 = int(numero1)
-else:
-    numero1 = input("Introducir primer numero: ")   
-    numero2 = input("Introducir segundo numero: ")
-    numero1 = int(numero1)
-    numero2 = int(numero2)
+    opciones = int(opciones)
+    if opciones == 0:
+        ejecutar=False
+
+    elif opciones == 5: 
+        numero1 = input("Introducir cantidad de numeros para la serie: ")   
+        numero1 = int(numero1)
+    else:
+        numero1 = input("Introducir primer numero: ")   
+        numero2 = input("Introducir segundo numero: ")
+        numero1 = int(numero1)
+        numero2 = int(numero2)
 
 
-if opciones == 1:
-    resultado = suma(numero1,numero2) 
-elif opciones == 2:
-    resultado = restar(numero1,numero2) 
-elif opciones == 5:
-    serie_fibonaci(numero1)
-
-if(opciones != 5):
-    print("resultado ",resultado)
+    if opciones == 1:
+        resultado = suma(numero1,numero2) 
+    elif opciones == 2:
+        resultado = restar(numero1,numero2) 
+    elif opciones == 5:
+        serie_fibonaci(numero1)
+                                         
+    if(opciones != 5  and opciones!=0):   
+        print("resultado ",resultado)
 
 
 
